68d717dc4af839667fdce4d968d04e11a9055509
garhett <60421166+garhett@users.noreply.github.com>
2020-10-09 22:16:28 -0600

    Fir commit!

--- src/Halladay/Hello.java
+++ src/Halladay/Hello.java
@@ -1,10 +1,8 @@
 package Halladay;
 
 public class Hello {
-    public static void main(String[] args) {
-        System.out.println("Hello my name is Sheila.");
-    }
-
-
-
+  public static void main(String[] args) {
+    System.out.println("Hello my name is Sheila.");
+    System.out.println("Hello Sheila, my name is Garhett!");
+  }
 }
